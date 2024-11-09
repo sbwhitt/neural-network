2c764541355176fcebef132086a31c0fe34abdf6
Samuel Whittenberger <sbw98978@gmail.com>
2024-11-09 11:22:36 -0500

    more cleanup

--- nn/utils.py
+++ nn/utils.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def show_images(images: list[list[np.ndarray]], titles: list[str]):
+  '''
+  from: https://www.kaggle.com/code/hojjatk/read-mnist-dataset
+  '''
   cols = 5
   rows = int(len(images)/cols) + 1
   plt.figure(figsize=(30,20))
@@ -15,17 +18,11 @@ def show_images(images: list[list[np.ndarray]], titles: list[str]):
       plt.title(title_text, fontsize=15);
     index += 1
 
-def activation(x: float) -> float:
-    return 1 if x > 0 else -1
-
 def tanh(x: float) -> float:
   return np.tanh(x)
 
 def d_tanh(tanh_x: float) -> float:
   return (1 - tanh_x**2)
-
-# def d_tanh(x: float) -> float:
-#   return 1 - (np.tanh(x)**2)
 
 def relu(x: float) -> float:
   return x if x > 0 else 0
@@ -35,6 +32,3 @@ def d_relu(relu: float) -> float:
 
 def softmax(z: list[float]) -> list[float]:
   return np.exp(z) / sum(np.exp(z))
-
-def sum_squared_errors(outcome: list[float], desired: list[float]) -> float:
-  return 0.5 * sum((np.array(outcome) - np.array(desired))**2)
